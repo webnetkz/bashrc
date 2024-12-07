0724930694a192441f36221a34503463ca551023
webnetkz <dbqqbq@gmail.com>
2024-12-07 19:36:31 +0500

    Change bash_profine to zshrc

--- install_mac.sh
+++ install_mac.sh
@@ -5,12 +5,12 @@ rsync -u .[^.]* ~/
 for file in .[^.]*; do
     if [ -f "$file" ]; then
         line_to_add=". ~/$file"
-        if ! grep -Fxq "$line_to_add" ~/.bash_profile; then
-            echo "$line_to_add" >> ~/.bash_profile
+        if ! grep -Fxq "$line_to_add" ~/.zshrc; then
+            echo "$line_to_add" >> ~/.zshrc
         fi
     fi
 done
 
-cd ~/ source .bashrc_profile
+cd ~/ source .zshrc
 echo "Success"
 
